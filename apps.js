0d83f80d09842ec161e8b5060cf42bd00c20a3a1
shahin Kobir <webthememer@gmail.com>
2023-01-17 23:15:56 +0600

    first commit

---
+++ apps.js
@@ -0,0 +1,114 @@
+// console.log('JavaScript is Running...')
+// var varType = 1000;
+// var varTypeTwo = 2000;
+
+// var varTotal = varType + varTypeTwo;
+
+// varTotal  = 5000;
+// console.log('First: ',varType);
+// var varType = 100;
+// console.log('sec:',varType);
+// var name = 'Jhon deo';
+
+// varTotalTwo = 3000;
+
+// console.log(varTotalTwo);
+// console.log(varTotal);
+// console.log(name);
+
+// let letType = 1000;
+// let letTypeTwo = 2000;
+
+// let letTypeTotal = letType + letTypeTwo;
+
+// console.log(letTypeTotal);
+
+// let letType = 200;
+
+// const array = ['john deo', 22, 017000000000000, 'web developer'];
+
+// array.push('johndeo@gmail.com');
+// array.push('45k');
+
+// // console.warn(array);
+
+
+// const obj = [
+//     {   
+//         id: '1',
+//         name: 'john deo',
+//         age: 22,
+//         phone: '017111111111111111',
+//         title: 'web developer',
+//         skill: ['HTML5',' CSS3', 'JAvascript', 'Bootstrap', 'Tailwind', 'React', 'Node js', 'exprass', 'mongodb']
+//     },
+//     {   
+//         id: '2',
+//         name: 'amit',
+//         age: 24,
+//         phone: '01711111345445111',
+//         title: 'web developer',
+//         skill: ['HTML5',' CSS3', 'JAvascript', 'Bootstrap', 'Tailwind', 'React', 'Node js', 'exprass', 'mongodb']
+//     },
+//     {   
+//         id: '3',
+//         name: 'john deo',
+//         age: 22,
+//         phone: '017111111111111111',
+//         title: 'web developer',
+//         skill: ['HTML5',' CSS3', 'JAvascript', 'Bootstrap', 'Tailwind', 'React', 'Node js', 'exprass', 'mongodb']
+//     },
+//     {   
+//         id: '4',
+//         name: 'amit',
+//         age: 24,
+//         phone: '01711111345445111',
+//         title: 'web developer',
+//         skill: ['HTML5',' CSS3', 'JAvascript', 'Bootstrap', 'Tailwind', 'React', 'Node js', 'exprass', 'mongodb']
+//     }
+// ];
+// const {id, name, age, phone, skill} = obj[3];
+
+// console.log(obj[0].name)
+
+// const arrayTwo = [
+//     ['HTML5',' CSS3', 'JAvascript', 'Bootstrap', 'Tailwind', 'React', 'Node js', 'exprass', 'mongodb'],
+//     ['HTML5',' CSS3', 'JAvascript', 'Bootstrap', 'Tailwind', 'React', 'Node js', 'exprass', 'mongodb'],
+//     ['HTML5',' CSS3', 'JAvascript', 'Bootstrap', 'Tailwind', 'React', 'Node js', 'exprass', 'mongodb'],
+// ]
+
+// console.log(arrayTwo);
+// oparetor Type
+
+// var a = 10;
+// var b = 20;
+
+// console.log(a==b)
+// Assignment Oparetor
+// Logic Oparetor
+// conditional Oparetor
+// comparisom Oparetor
+//string oparetor
+
+// var  input = prompt('Enter Yor Temperature in Celcius: '); //user Input
+
+// var result = 9/5* input + 32; // calculatrion 
+
+// alert('Fahrenheit: '+  result  + "deg"); // output 
+
+// console.log('Task Complate'); // console log
+
+// var age = prompt('Enter Your Age: ');
+
+// var result = age < 18 ? `you are a baby your age is ${age}` :  `you are not a baby your age is ${age}`;
+
+// console.log(result);
+// alert(result);
+
+// var age = prompt('Enter Your Age: ');
+
+// if(age < 18){
+//     alert(`you are a baby your age is ${age}`);
+// }else{
+//     alert(`you are not a baby your age is ${age}`);
+// }
